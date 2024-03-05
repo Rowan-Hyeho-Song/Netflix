fe5c14adce3f227c073954b30c2c3d8fa3ca045c
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-03-05 16:18:30 +0900

    Change API KEY load
    api 키를 환경 변수로 지정하여 외부에 노출되지 않도록 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "http://api.themoviedb.org/3/",
     params: {
-        api_key: "47692baa39c263037a837c857b7c990d",
-        language: "ko-KR",
+        api_key: import.meta.env.VITE_API_KEY,
     },
 });
 
