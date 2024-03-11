748bcd6f5b0a342699a356861d1778d155b8e77e
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-03-11 23:47:36 +0900

    Update support.lang.jsx
    - 빌드냈을때 경로 로딩에 문제가 있는 코드 수정

--- src/constants/lang/support.lang.jsx
+++ src/constants/lang/support.lang.jsx
@@ -1,6 +1,9 @@
+import usa from "@assets/flag/usa_flag.svg";
+import ko from "@assets/flag/south_korea_flag.svg";
+
 const supportLang = [
-    { key: "en-US", icon: "src/assets/flag/usa_flag.svg" },
-    { key: "ko-KR", icon: "src/assets/flag/south_korea_flag.svg" },
+    { key: "en-US", icon: usa },
+    { key: "ko-KR", icon: ko },
 ];
 
 export default supportLang;
