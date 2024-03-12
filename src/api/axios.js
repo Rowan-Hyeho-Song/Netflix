75030384ca58e1f891988f94d30dc0b646ff6623
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-03-12 10:35:44 +0900

    Update axios.js
    배포 환경변수 잘 들어오는 것을 확인 log 제거

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log("API KEY CHECK >>> ", import.meta.env.VITE_API_KEY);
 const instance = axios.create({
     baseURL: "http://api.themoviedb.org/3/",
     params: {
