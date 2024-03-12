063e4a5a722301c080f86575d0e345a41565d947
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-03-12 10:25:23 +0900

    Update axios.js
    배포환경에서 환경변수가 제대로 들어왔나 확인을 위해 임시 log 추가

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+console.log("API KEY CHECK >>> ", import.meta.env.VITE_API_KEY);
 const instance = axios.create({
     baseURL: "http://api.themoviedb.org/3/",
     params: {
