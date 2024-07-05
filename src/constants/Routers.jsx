b5334449ea6c6c88bb1f94e22257a32b95b9a669
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-07-05 11:09:52 +0900

    Update Routers.jsx
    - change router paths

--- src/constants/Routers.jsx
+++ src/constants/Routers.jsx
@@ -3,9 +3,9 @@ import Series from "@pages/Series";
 import Movie from "@pages/Movie";
 
 const routers = [
-    { name: "tab.home", path: "/", element: <Home /> },
-    { name: "tab.series", path: "/series", element: <Series /> },
-    { name: "tab.movie", path: "/movie", element: <Movie /> },
+    { name: "tab.home", path: "/Netflix", element: <Home /> },
+    { name: "tab.series", path: "/Netflix/series", element: <Series /> },
+    { name: "tab.movie", path: "/Netflix/movie", element: <Movie /> },
 ];
 
 export { routers };
